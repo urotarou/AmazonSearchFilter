8a79eaebae4180ccef98b03168876c13c6a19f7b
urotarou <devak9kflab@gmail.com>
2024-12-09 21:12:31 +0900

    全面的にリファクタリング

--- js/background.js
+++ js/background.js
@@ -1,25 +1,39 @@
-var targetTabId
+const VALID_URLS = ["https://www.amazon.co.jp/"];
+const QUERY_AMAZON_ONLY = "&rh=p_6%3AAN1VRQENFRJN5";
+
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+    const tab = await chrome.tabs.get(activeInfo.tabId);
+    
+    updateActionState(tab);
+});
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status !== "complete") return
+    if (changeInfo.status !== "complete") return;
 
-    if (tab.url.startsWith("https://www.amazon.co.jp/")) {
-        console.log("Hello Amazon!!")
+    updateActionState(tab);
+});
 
-        targetTabId = tabId
-        chrome.action.enable(tabId)
+chrome.action.onClicked.addListener((tab) => {
+    if(tab.url.includes(QUERY_AMAZON_ONLY)) return;
+
+    const newUrl = tab.url + QUERY_AMAZON_ONLY;
+
+    console.log("old: " + tab.url);
+    console.log("new: " + newUrl);
+
+    chrome.tabs.update({"url": newUrl});
+});
+
+function updateActionState(tab) {
+    const isValidUrl = VALID_URLS.some((validUrl) => {
+        return tab.url.startsWith(validUrl);
+    });
+
+    if (tab.url && isValidUrl) {
+        console.log(`enable: [${tab.title}](${tab.url})`);
+        chrome.action.enable(tab.id);
+    } else {
+        console.log(`disable: [${tab.title}](${tab.url})`);
+        chrome.action.disable(tab.id);
     }
-})
-
-chrome.action.onClicked.addListener(() => {
-    chrome.tabs.get(parseInt(targetTabId), (tab) => {
-        if(tab.url.includes("AN1VRQENFRJN5")) return
-
-        var newUrl = tab.url + "&emi=AN1VRQENFRJN5"
-
-        console.log("old: " + tab.url)
-        console.log("new : " + newUrl)
-
-        chrome.tabs.update({"url": newUrl})
-    })
-})
+}
